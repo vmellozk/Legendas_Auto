72b95c23ffd9185ccda5b617193b031ec3830714
Victor Mello <contato.devictormello@gmail.com>
2025-05-16 20:59:40 -0300

    adicionando caminho do ffmpeg e passando suporte do pyinstaller

--- core/youtube.py
+++ core/youtube.py
@@ -1,10 +1,21 @@
 import yt_dlp
+import sys
+import os
+
+def resource_path(relative_path):
+    # Para suportar PyInstaller
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
 
 def download_audio_youtube(video_url, output_path):
     try:
+        ffmpeg_dir = resource_path(os.path.join("ffmpeg", "bin"))
+        
         ydl_opts = {
             'format': 'bestaudio/best',
             'outtmpl': output_path,
+            'ffmpeg_location': ffmpeg_dir,
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
